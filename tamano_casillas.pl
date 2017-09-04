41bb483204d73ec8040f9279d6cffbfb970258dc
Miguel Coa M <mcoa@mcoa-new-air.home>
2017-09-04 00:19:38 -0300

    first commit

---
+++ tamano_casillas.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+my @hostname = `/opt/zimbra/bin/zmprov gas`;
+#print @hostname
+
+foreach (@hostname) {
+	my @quotas = `/opt/zimbra/bin/zmprov gqu @hostname`;
+#	print @quotas
+print "-------EMAIL------------QUOTA USAGE---------------QUOTA SET------\n";
+
+foreach my $quotaline(@quotas)
+	{
+		chop($quotaline);
+		my ($email, $quota, $usage) = split(' ', $quotaline);
+		$quota=utils_convert_bytes_to_optimal_unit($quota);
+		$usage=utils_convert_bytes_to_optimal_unit($usage);
+		print "$email - $usage - $quota\n";
+	}
+
+sub utils_convert_bytes_to_optimal_unit{
+	my($bytes) = @_;
+	return '' if $bytes eq '';
+	my $size;
+	$size = $bytes . ' Bytes' if $bytes < 1024;
+	$size = sprintf("%.2f", $bytes/1024) . ' KB' if $bytes >= 1024 && $bytes < 1048576;
+	$size = sprintf("%.2f", $bytes/1048576) . ' MB' if $bytes >= 1048576 && $bytes < 1073741824;
+	$size = sprintf("%.2f", $bytes/1073741824) . ' GB' if $bytes >= 1073741824 && $bytes < 1099511627776;
+	$size = sprintf("%.2f", $bytes/1099511627776) . ' TB' if $bytes >= 1099511627776;
+	return $size;
+ }
+}
